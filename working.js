13c9da34b4de4910cf06a5fe643ef86f3fc6999d
tejashshrestha <tejash@loyalytics.in>
2020-10-04 01:09:37 +0545

    1) Added SVG for all weathers 2) Edited the Background and colors

--- working.js
+++ working.js
@@ -1,37 +1,48 @@
 // On-load
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
 	let longitude;
 	let latitude;
 	let tempDescription = document.querySelector(".temperature-description");
 	let tempDegree = document.querySelector(".temperature-degree");
 	let locationTimezone = document.querySelector(".location-timezone");
 	let tempSection = document.querySelector(".temperature");
+	let dayMood = document.querySelector(".temperature-description-mood");
 	const tempSpan = document.querySelector(".temperature span");
 
-	// Getting position of location
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(position=>{
-			longitude = position.coords.longitude;
-			latitude = position.coords.latitude;
 
-			const proxy = 'http://cors-anywhere.herokuapp.com/';
-			const api = `${proxy}https://api.darksky.net/forecast/c9df135483f3f71511ddf718b1fa3cde/${latitude},${longitude}`;
+	function setWeatherDetails(position) {
+		longitude = position.coords.longitude;
+		latitude = position.coords.latitude;
 
-			fetch(api)
-			.then(response=>{
+		const proxy = 'http://cors-anywhere.herokuapp.com/';
+		const api = `${proxy}https://api.darksky.net/forecast/c9df135483f3f71511ddf718b1fa3cde/${latitude},${longitude}`;
+
+		fetch(api)
+			.then(response => {
 				return response.json();
 			})
-			.then(data=>{
+			.then(data => {
 				console.log(data);
-				const { temperature, summary, icon} = data.currently;
+				const { time, temperature, summary, icon } = data.currently;
 				tempDegree.textContent = temperature;
 				tempDescription.textContent = summary;
 				locationTimezone.textContent = data.timezone;
+				let dayTime = 'day';
 
-				let celcius = (temperature - 32) * ( 5 / 9 );
+				if ((new Date(time).getHours() + data.offset) < 12) {
+					dayMood.textContent = 'Good Morning';
+				} else if ((new Date(time).getHours() + data.offset) < 18) {
+					dayMood.textContent = 'Good Evening';
+				} else if ((new Date(time).getHours() + data.offset) > 18) {
+					dayMood.textContent = 'Good Night';
+					dayTime = 'night';
+
+				}
+
+				let celcius = (temperature - 32) * (5 / 9);
 
 				//Icon setting
-				setIcon(icon, document.querySelector(".icon"));
+				setIcon(icon, dayTime);
 
 				//convert celcius and farenheit
 				tempSection.addEventListener("click", () => {
@@ -39,22 +50,55 @@ window.addEventListener('load',()=>{
 						tempSpan.textContent = "C";
 						tempDegree.textContent = Math.floor(celcius);
 					}
-					else{
+					else {
 						tempDegree.textContent = temperature;
 						tempSpan.textContent = "F";
 					}
 				});
 			});
-		});
-	}
-	else{
-		h1.textContent ="Weather Out of Track";
 	}
 
-	function setIcon(icon, iconID){
-		const skycons = new Skycons({color: "white"});
-		const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-		skycons.play();
-		return skycons.set(iconID, Skycons[currentIcon]);
+	const position = {
+		coords: {
+			longitude: 0,
+			latitude: 0,
+		}
+	}
+	setWeatherDetails(position);
+	// Getting position of location
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(position => {
+			setWeatherDetails(position);
+		});
+	}
+	else {
+		h1.textContent = "Weather Out of Track";
+	}
+
+	function setIcon(icon, dayTime) {
+		const baseURL = './assets/images/animated/';
+		if (icon.includes('cloud') || icon.includes('fog')) {
+			if (icon.includes('day') || dayTime == 'day') {
+				document.getElementById('weatherIcon').src = baseURL + 'cloudy.svg';
+			} if (icon.includes('night') || dayTime == 'night') {
+				document.getElementById('weatherIcon').src = baseURL + 'cloudy-night-1.svg';
+			}
+		} else if (icon.includes('wind')) {
+			document.getElementById('weatherIcon').src = baseURL + 'cloudy-day-1.svg';
+		} else if (icon.includes('clear')) {
+			if (icon.includes('day' || dayTime === 'day')) {
+				document.getElementById('weatherIcon').src = baseURL + 'day.svg';
+			} if (icon.includes('night' || dayTime === 'night')) {
+				document.getElementById('weatherIcon').src = baseURL + 'night.svg';
+			}
+		} else if (icon.includes('snow')) {
+			document.getElementById('weatherIcon').src = baseURL + 'snow-mid.svg';
+		} else if (icon.includes('rain')) {
+			document.getElementById('weatherIcon').src = baseURL + 'rainy-mid.svg';
+		} else if (dayTime === 'day') {
+			document.getElementById('weatherIcon').src = baseURL + 'day.svg';
+		} else if (dayTime === 'night') {
+			document.getElementById('weatherIcon').src = baseURL + 'night.svg';
+		}
 	}
 });
